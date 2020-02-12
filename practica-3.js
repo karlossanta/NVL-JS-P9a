04f8ebfc5da9eff66745854d4cf331dc7cf2071e
Carlos <karlosssanta@gmail.com>
2020-02-12 14:39:57 +0100

    manejo de arrays

---
+++ practica-3.js
@@ -0,0 +1,12 @@
+var array = ["hola", "mi", "nombre", "es", "Carlos"];
+console.log(array)
+var longitudArray = array.length
+console.log("Longitud del array: " + longitudArray);
+var longitudElemento5 = array[4].length;
+console.log("Longitud del quinto elemento: " + longitudElemento5);
+var elemento3 = array[2];
+console.log("Tercer elemento del array: " + elemento3);
+alert("Tercer elemento del array: " + elemento3);
+var elemento2del3 = elemento3[1];
+console.log("Segundo elemento dentro del tercer elemento del array: " + elemento2del3);
+alert("Segundo elemento dentro del tercer elemento del array: " + elemento2del3);
